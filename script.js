5adde5ad611c7ec5fb6e769f3b136243ce21c96c
Isaiah0jones <Jonesisaiah101@gmail.com>
2025-12-17 23:54:55 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scrolling (same as first website)
-document.querySelectorAll('a[href^="#"]').forEach(a => {
-  a.addEventListener('click', (e) => {
-    e.preventDefault();
-    const target = document.querySelector(a.getAttribute('href'));
-    if (target) target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
